f7dcaa08f924797a49be48ba9fe5f5c870bd8a50
이도건 <dohkun@idogeon-ui-MacBookPro.local>
2022-08-12 15:41:59 +0900

    0812 commit

--- CBook/Part02PointerAndArray/chal4.c
+++ CBook/Part02PointerAndArray/chal4.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
-
-    char* word;
-    int size;
-    
+    //회문인지 체크하는 것.
+    char word[30];
+    int i = 0;
+    int length = 0;
+    int flag = 1;
     scanf("%s",word);
-    size = sizeof(word);
-    printf("%d\n",size);
+    length = strlen(word);
+    length -= 1;
+    //printf("length : %d\n",length);
+    printf("입력하신 단어는 %s 입니다. \n",word);
 
+    for(i=0;i<(length/2);i++){
+        //printf("\nword[i] = %c , word[length-i] = %c\n", word[i],word[length-i]);
+        if(word[i] == word[length-i]){
+            continue;
+        }else{
+            flag = 0;
+            
+            break;
+        }
+        
+    }
+
+    if(flag==1){
+        printf("입력하신 단어는 회문입니다.");
+        
+    }else{
+        printf("입력하신 단어는 회문이 아닙니다.");
+
+    }
+    return 0;
+
+    
+
+
+
+    
 }
